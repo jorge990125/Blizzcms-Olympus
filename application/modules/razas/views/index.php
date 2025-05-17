56f3a2c5fed24668749e5c789fc48f2c467d295e
RëXol <66186993+jorge990125@users.noreply.github.com>
2025-05-17 16:56:08 -0400

    Update index.php

--- application/modules/razas/views/index.php
+++ application/modules/razas/views/index.php
@@ -1,11 +1,11 @@
 <section class="uk-section uk-section-small" data-uk-height-viewport="expand: true">
     <div class="uk-container uk-card uk-card-body uk-card-default">	
         <h2 style="text-align: center;">Recibe a dos nuevas razas en Wotlk - Pixie</h2>
-	    <img src="/application/themes/default/assets/images/razas/01.jpg">
+	    <img src="/application/themes/olympus/assets/images/razas/01.jpg">
 		<p>Los héroes se unen a la lucha para experimentar un Azeroth totalmente cambiado y defenderlo de la devastación total con un sistema de aumento de nivel más sencillo y acelerado.</p>
 		<p>Dos nuevas razas (los huargen y los goblins) se unen a la Alianza y a la Horda, y ambos aportan algo único a Azeroth.</p>
         <h3>Los huargen</h3>
-		<img src="/application/themes/default/assets/images/razas/02.png" style="width: 400px; height: 250px;">
+		<img src="/application/themes/olympus/assets/images/razas/02.png" style="width: 400px; height: 250px;">
         <p>Detrás de la formidable Muralla de Cringris, una maldición terrible ha transformado a algunos de los leales ciudadanos del aislado reino de Gilneas en bestias lupinas conocidas como huargen. Incapaces de curar su aflicción y asediados por los temibles Renegados, estos huargen han aprendido a controlar su ferocidad gracias a la ayuda de los elfos de la noche. Los huargen ahora están listos para liberar a la bestia interior al instante comocaballeros de la muerte, druidas, cazadores, magos, sacerdotes, pícaros, brujos, y guerreros en defensa de la Alianza.</p>
        	
         <h3>Atributos raciales</h3>
@@ -16,11 +16,11 @@
 		<p>🔹 Saña (pasivo): aumenta la probabilidad de golpe crítico un 1%.</p>
 
         <h3>Montura racial: Caballo de montaña</h3>		
-		<img src="/application/themes/default/assets/images/razas/03.jpg" style="width: 400px; height: 250px;">
+		<img src="/application/themes/olympus/assets/images/razas/03.jpg" style="width: 400px; height: 250px;">
         <p>Estos caballos salvajes han servido a los gilneanos desde antes de la Primera Guerra. Los caballos de montaña son conocidos por su fuerza y su comportamiento feroz, por sentirse a gusto con sus jinetes y por ser propensos a volver a su naturaleza salvaje.</p>
         
         <h3>Los goblins</h3>
-		<img src="/application/themes/default/assets/images/razas/04.png" style="width: 400px; height: 250px;">
+		<img src="/application/themes/olympus/assets/images/razas/04.png" style="width: 400px; height: 250px;">
         
 		<h3>Atributos raciales</h3>
         <p>🔹 Los mejores precios del mercado (pasivo): recibes siempre el mejor descuento de oro posible independientemente de tu reputación.</h3>
@@ -31,7 +31,7 @@
 		<p>🔹 El tiempo es oro (pasivo): aumenta un 1% el ataque y la velocidad de lanzamiento.</h3>
 		
 		<h3>Montura racial: Triciclo</h3>
-		<img src="/application/themes/default/assets/images/razas/05.png" style="width: 400px; height: 250px;">
+		<img src="/application/themes/olympus/assets/images/razas/05.png" style="width: 400px; height: 250px;">
         <p>El notablemente anciano Jibidiah “Sin dedos” Gragglefutz creó este robusto triciclo. Años de accidentes de producción han dado como resultado este vehículo veloz, duradero y que casi nunca explota, lo que lo ha hecho muy popular entre los goblins.</p>
 		</div>
 </section>
